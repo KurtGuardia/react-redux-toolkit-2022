74c0831e2f265a7c5afd93cce7e86ae2d49b917c
Kurt Guardia <kurtguardia@gmail.com>
2022-03-05 22:14:18 +1100

    NavBar, Home and Counter components done

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  let clickInc = () => {
+    setCount((oldState) => setCount(oldState + 1));
+  };
+  let clickDec = () => {
+    setCount((oldState) => setCount(oldState - 1));
+  };
+  let clickby5 = () => {
+    setCount((oldState) => setCount(oldState + 5));
+  };
+
   return (
     <React.Fragment>
-      <h2>Counter</h2>
+      <div className='container mt-5'>
+        <div className='row'>
+          <div className='col-md-4'>
+            <div className='card'>
+              <div className='card-body'>
+                <p className='h3'>{count}</p>
+                <button onClick={clickInc} className='btn btn-success m-1'>
+                  Increment
+                </button>
+                <button onClick={clickDec} className='btn btn-warning m-1'>
+                  Decrement
+                </button>
+                <button onClick={clickby5} className='btn btn-danger m-1'>
+                  Increment by 5
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 const Home = () => {
   return (
     <React.Fragment>
-      <h2>Home</h2>
+      <div className='container mt-5'>
+        <div className='row'>
+          <div className='col text-center mt-5'>
+            <img
+              src='https://media.vlpt.us/images/chaerin00/post/bf952beb-2b78-4f33-a2f6-f2f13e47a637/redux_tookit.png?w=768'
+              alt='Redux'
+            />
+            <p className='h3 display-3'>
+              Crash Course <span className='fw-bold'>2022</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
