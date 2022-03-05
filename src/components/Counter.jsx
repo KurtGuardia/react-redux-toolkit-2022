b1b7276c41a778592390d1304d67e4c5fa74a0dc
Kurt Guardia <kurtguardia@gmail.com>
2022-03-05 11:54:13 +0100

    Setup

---
+++ src/components/Counter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Counter = () => {
+  return (
+    <React.Fragment>
+      <h2>Counter</h2>
+    </React.Fragment>
+  );
+};
+
+export default Counter;
---
+++ src/components/Employees.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Employees = () => {
+  return (
+    <React.Fragment>
+      <h2>Employees</h2>
+    </React.Fragment>
+  );
+};
+
+export default Employees;
